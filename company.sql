cf9b6404a936a3eecc090d9f38ccc74f6f44bafe
lz <769950833@qq.com>
2013-09-14 11:01:49 +0800

    modify company.PDM company.sql

--- company.sql
+++ company.sql
@@ -8,14 +8,13 @@ create table tbl_product_category
    id                   integer not null auto_increment,
    name                 varchar(20) not null,
    type                 tinyint(1) not null comment '一级分类=1，二级分类=2，三级分类=3',
-   parent_id            integer not null comment '一级分类上级分类=0',
-   is_show              tinyint(1) not null default 0 comment '是否显示该分类，0=显示，1=隐藏，默认0',
+   parent_id            integer not null default 0 comment '一级分类上级分类=0,默认=0',
+   is_show              tinyint(1) not null default 0 comment '是否显示该分类，1=显示，0=隐藏，默认1',
    order_num            integer not null comment '分类的排序，默认=1',
    create_time          datetime not null,
    update_time          datetime not null,
    primary key (id)
 )
-type = InnoDB;
 
 drop table if exists tbl_product;
 
@@ -28,12 +27,9 @@ create table tbl_product
    name                 varchar(50) not null,
    brand                varchar(50) not null,
    pic                  varchar(200) not null,
-   onshow               tinyint(1) not null default 0 comment '0=上架，1=下架，默认0',
-   summary              varchar(200) not null comment '产品概要',
+   is_show              tinyint(1) not null default 0 comment '0=上架，1=下架，默认0',
+   summary              varchar(200) comment '产品概要',
    description          text not null comment '产品详情',
-   category_first       integer not null comment '一级分类',
-   category_second      integer not null comment '二级分类',
-   category_third       integer not null comment '三级分类',
    create_time          datetime not null,
    update_time          datetime not null,
    primary key (id)
@@ -52,6 +48,21 @@ create table tbl_company
    contact              varchar(20) not null comment '联系人',
    address              varchar(100) not null comment '地址',
    create_time          datetime not null,
+   primary key (id)
+);
+
+drop table if exists tbl_product_category_relation;
+
+/*==============================================================*/
+/* Table: tbl_product_category_relation   产品分类关联表                      */
+/*==============================================================*/
+create table tbl_product_category_relation
+(
+   id                   integer not null,
+   category_id          integer not null comment '分类id',
+   product_id           integer not null comment '产品id',
+   create_time          datetime not null comment '创建时间',
+   update_time          datetime not null comment '修改时间',
    primary key (id)
 );
 
