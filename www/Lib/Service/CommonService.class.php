fbfc0eb76a28c2409fd81c60a46ae80550625874
fengchuan <fengkechuan@gmail.com>
2013-09-16 09:52:42 +0800

    update CommonService

--- www/Lib/Service/CommonService.class.php
+++ www/Lib/Service/CommonService.class.php
@@ -2,17 +2,17 @@
 	import("ORG.Util.Page");
 	class CommonService{
 		//默认值
-		protected static $default=array('condition'=>"",'page'=>1,'pageSize'=>DEFAULT_PAGE_SIZE);
+		protected $default=array('condition'=>"",'page'=>1,'pageSize'=>DEFAULT_PAGE_SIZE);
 		
-		public static function setDefault($key,$value){
-			static::$default[$key]=$value;
+		public function setDefault($key,$value){
+			$this->default[$key]=$value;
 		}
 		/**
 		 * 
 		 * 总条数
 		 * @param array $options
 		 */
-		public static function count($options){
+		public function count($options){
 			return 0;
 		}
 		/**
@@ -20,10 +20,10 @@
 		 * 分页页码
 		 * @param array $options
 		 */
-		public static function showPage($options){
-			!isset($options['condition']) && $options['condition']=static::$default['condition'];
-			!isset($options['page']) && $options['page']=static::$default['page'];
-			!isset($options['pageSize']) && $options['pageSize']=static::$default['pageSize'];
+		public function showPage($options){
+			!isset($options['condition']) && $options['condition']=$this->default['condition'];
+			!isset($options['page']) && $options['page']=$this->default['page'];
+			!isset($options['pageSize']) && $options['pageSize']=$this->default['pageSize'];
 			$count=static::count($options['condition']);
 			$page=new Page($count,$options['pageSize']);
 			$theme='%first% %upPage% %linkPage% %downPage% %end%';
