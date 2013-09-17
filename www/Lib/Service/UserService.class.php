70c1a5f22eea6494b426f3a642c229d18169c92c
fengchuan <fengkechuan@gmail.com>
2013-09-17 12:57:55 +0800

    user

--- www/Lib/Service/UserService.class.php
+++ www/Lib/Service/UserService.class.php
@@ -2,8 +2,16 @@
 	import("@.Service.CommonService");
 	class UserService extends CommonService{
 		
-		public function count($options){
-			return 0;
+		public function count($options = array()){
+			$sql="";
+			!isset($options['user_group_id']) && $options['user_group_id']=0;
+			false===strpos($options['user_group_id'], ",")?
+				$sql="user_group_id=".$options['user_group_id']:$sql="user_group_id in (".$options['user_group_id'].")";
+			!isset($options['status']) && $options['status']=0
+				&& $sql.=" and status=".$options['status'];
+			isset($options['condition']) && $sql.=$options['condition'];
+			$count=M("User")->where($sql)->count();
+			return $count;
 		}
 		public function createUser($user){
 			return M("User")->add($user);
@@ -11,32 +19,52 @@
 		public function updateUser($user){
 			return M("User")->save($user);
 		}
-		public function findUser($user){
-			$ret=null;
-			!isset($user['user_group_id']) &&$user['user_group_id']=0;
-			isset($user['id']) && $ret=M("User")->where("user_group_id=".$user['user_group_id'])->find($user['id']);
-			!isset($user['id']) && isset($user['name'])
-				&& $ret=M("User")->where("name='".$user['name']."' and user_group_id=".$user['user_group_id'])->find();
-			!isset($user['id']) && isset($user['email'])
-				&& $ret=M("User")->where("email='".$user['email']."' and user_group_id=".$user['user_group_id'])->find();
+		public function findUser($user = array()){
+			!isset($user['user_group_id']) && $user['user_group_id']=0;
+			!isset($user['status']) && $user['status']=0;
+			$sql="user_group_id=".$user['user_group_id']." and status=".$user['status'];
+			isset($user['id']) && $sql.=" and id=".$user['id'];
+			isset($user['name']) && $sql.=" and name='".$user['name']."'";
+			isset($user['email']) && $sql.=" and email='".$user['email']."'";
+			$ret=M("User")->where($sql)->find();
 			return $ret;
 		}
-		public function findUsers($options){
-			
+		/**
+		 * 
+		 * @todo:搜索条件待完成
+		 * @param array $options
+		 * @return array
+		 */
+		public function findUsers($options = array()){
+			$ret=null;
+			!isset($options['user_group_id']) && $options['user_group_id']=0;
+			!isset($options['status']) && $options['status']=0;
+			$sql="user_group_id=".$options['user_group_id']." and status=".$options['status'];
+			$ret=M("User")->where($sql)->order("id desc")->select();
+			return $ret;
 		}
-		public function deleteUser($user){
+		/**
+		 * 逻辑删除
+		 * @param unknown_type $user
+		 * @return unknown
+		 */
+		public function deleteUser($user = array()){
 			$flag=false;
-			(isset($user['id']) && $flag=M("User")->delete($user['id']))
-				||	(isset($user['name']) && $flag=M("User")->where("name = '".$user['name']."'")->delete()) ;
+			$sql="";
+			isset($user['id'])? $sql="id=".$user['id']:
+				(isset($user['name']) && $sql="name = '".$user['name']."'");
+			!empty($sql) && $flag=M("User")->where($sql)->setField("status",1);
 			return $flag;
 		}
-		public function deleteUsers($options){
+		public function deleteUsers($options = array()){
 			$flag=false;
-			(isset($options['ids']) && $flag=M("User")->where("id in (".$options['ids'].")")->delete())
-				|| (isset($options['names']) && $flag=M("User")->where("name in (".$options['names'].")")->delete());
+			$sql="";
+			isset($options['ids'])? $sql="id in (".$options['ids'].")":
+				( isset($options['names']) && $sql="name in (".$options['names'].")" );
+			!empty($sql) && $flag=M("User")->where($sql)->setField("status",1);
 			return $flag;
 		}
-		public function isExisted($user){
+		public function isExisted($user = array()){
 			$sql="";
 			isset($user['name']) && !isset($user['email']) && $sql=" name ='".$user['name']."'";
 			isset($user['email']) && !isset($user['name']) && $sql=" email ='".$user['email']."'";
